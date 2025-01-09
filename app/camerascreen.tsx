eca0c80a9b74f035986f359e5bd0d72b73b90be3
Sunil Gopal C V <sunilgopal63@gmail.com>
2025-01-09 08:31:53 +0530

    Completed tailwind setup and finished design of welcome page

--- app/camerascreen.tsx
+++ app/camerascreen.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, TouchableOpacity, Text, ScrollView, Image } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useAppContext } from "../contexts/AppContext";
 import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Entypo from "@expo/vector-icons/Entypo";
+
+import { images } from "../constants";
 
 export default function CameraScreen() {
   const { setImageUri, setResult } = useAppContext();
@@ -34,13 +39,45 @@ export default function CameraScreen() {
   };
 
   return (
-    <View style={styles.container}>
-      <Button title="Capture Image" onPress={captureImage} />
-      <Button title="Upload Image" onPress={pickImage} />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Text className="text-white font-pbold text-center text-2xl">
+            Upload an Image
+          </Text>
+
+          <Image
+            source={images.group_cards}
+            className="max-w-[380px] w-full h-[298px] my-16"
+            resizeMode="contain"
+          />
+
+          <View className="flex flex-row items-center justify-center gap-6">
+            <TouchableOpacity
+              onPress={captureImage}
+              className="bg-black-100 rounded-xl min-h-[200px] min-w-[170px] flex justify-center items-center border-dashed border-2 border-gray-200 gap-3"
+            >
+              <FontAwesome name="camera" size={54} color="#d9d9db" />
+              <Text className="text-white max-w-[120px] text-center font-pregular text-lg">
+                ಕ್ಯಾಮೆರಾ ಆನ್
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={pickImage}
+              className="bg-black-100 rounded-xl min-h-[200px] min-w-[170px] flex justify-center items-center border-dashed border-2 border-gray-200 gap-3"
+            >
+              <Entypo name="folder-images" size={54} color="#d9d9db" />
+              <Text className="text-white max-w-[120px] text-center  font-pregular text-lg">
+                ಗ್ಯಾಲರಿ ಅಪ್ಲೋಡ್
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <Text className="text-white font-pregular text-sm mt-10">
+            ಜೆಎಸ್‌ಎಸ್‌ಎಟಿಇ ಬೆಂಗಳೂರು ಪ್ರಾಯೋಜಿತ
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-});
--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,63 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import { useRouter } from "expo-router";
+import { Redirect, useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
+import CustomButton from "@/components/CustomButton";
 
 export default function WelcomeScreen() {
   const router = useRouter();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Harry-Barbery Predictor!</Text>
-      <Text style={styles.subtitle}>
-        Capture or upload an image of a sheep to classify its breed.
-      </Text>
-      <Button
-        title="Get Started"
-        onPress={() => router.push("/camerascreen")}
-      />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <View className="flex flex-row gap-4 items-center">
+            <Image
+              source={images.logo}
+              className="w-[80px] h-[80px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-col items-center">
+              <Text className="font-pbold text-white text-3xl">JAGRUTHI</Text>
+              <Text className="text-white font-pregular text-sm">
+                ಜಾನುವಾರುಗಳ ಗುರುತಿಸಿ
+              </Text>
+            </View>
+          </View>
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+
+          <View className="flex items-center mt-5">
+            <Text className="text-2xl text-white font-pbold text-center mt-3 px-8">
+              Identifying Breeds, Empowering Care with{" "}
+              <Text className="text-secondary-200">JAGRUTHI</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px]"
+              resizeMode="contain"
+            />
+          </View>
+          {/* Continue with Email Button */}
+          <CustomButton
+            title="ಜಾನುವಾರುಗಳ ಗುರುತಿಸಿ"
+            handlePress={() => router.push("/camerascreen")}
+            containerStyles="w-full mt-12"
+          />
+
+          <Text className="text-white font-pregular text-sm mt-5">
+            ಜೆಎಸ್‌ಎಸ್‌ಎಟಿಇ ಬೆಂಗಳೂರು ಪ್ರಾಯೋಜಿತ
+          </Text>
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-    textAlign: "center",
-  },
-  subtitle: { fontSize: 16, textAlign: "center", marginBottom: 40 },
-});
